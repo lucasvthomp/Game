bcd061447148ab499dec6d4b63430b2e05cbcbfe
Lucas <lvt655@gmail.com>
2026-08-24 19:09:10 -0400

    Add small lancha visual asset reference

--- client/src/lib/boat-media.ts
+++ client/src/lib/boat-media.ts
@@ -1,9 +1,9 @@
 export const BOAT_MEDIA = {
   coast: "/images/marcamar-map-coast.svg",
   route: "/images/marcamar-map-route.svg",
+  smallLancha: "/images/marcamar-small-lancha.jpg",
   dock: "https://images.pexels.com/photos/37010028/pexels-photo-37010028.jpeg?auto=compress&cs=tinysrgb&fit=crop&w=1200",
   boarding: "https://images.pexels.com/photos/32779986/pexels-photo-32779986.jpeg?auto=compress&cs=tinysrgb&fit=crop&w=1200",
   commute: "https://images.pexels.com/photos/17081934/pexels-photo-17081934.jpeg?auto=compress&cs=tinysrgb&fit=crop&w=1200",
   passenger: "/images/marcamar-ferry-passenger.jpg",
 } as const;
-
